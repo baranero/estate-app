b0643157a28de15ea080c76b1bb9826b4e96d009
baranero <jakub.baran2@gmail.com>
2023-10-05 13:45:12 +0200

    connect to database

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,22 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
+
+const mongoUri = process.env.MONGO || "mongodb://localhost:27017/mydb";
+
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    
-})
+  console.log("Server is running on port 3000");
+});
