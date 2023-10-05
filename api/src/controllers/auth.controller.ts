2dc13b1749218a90eee2331429529b6bf6b54d0c
baranero <jakub.baran2@gmail.com>
2023-10-05 18:23:44 +0200

    create a middleware and a function to handle possible errors

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -1,8 +1,12 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import User from "../models/user.model";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req: Request, res: Response) => {
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -11,6 +15,6 @@ export const signup = async (req: Request, res: Response) => {
     await newUser.save();
     res.status(201).json("User created successfully");
   } catch (err: any) {
-    res.status(500).json(err.message);
+    next(err);
   }
 };
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,5 @@
-import express from "express";
-import mongoose from "mongoose";
+import express, { NextFunction, Request, Response } from "express";
+import mongoose, { Error } from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route'
 import authRouter from './routes/auth.route'
@@ -27,3 +27,14 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error'
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+})
