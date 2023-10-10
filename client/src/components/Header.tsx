b1b6276e592c1ce17e038319119476b41fc98b8b
baranero <jakub.baran2@gmail.com>
2023-10-10 18:51:15 +0200

    update the header and make the profile page private

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,15 +1,19 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
 
 const Header = () => {
+  const { currentUser } = useSelector((state: RootState) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
-        <Link to='/'>
-            <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-              <span className="text-slate-500">Jakub</span>
-              <span className="text-slate-700">Baran</span>
-            </h1>
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Jakub</span>
+            <span className="text-slate-700">Baran</span>
+          </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
@@ -22,15 +26,25 @@ const Header = () => {
           <FaSearch className="text-slate-600" />
         </form>
         <ul className="flex gap-4">
-            <Link to='/'>
-                <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">Home</li>
-            </Link>
-            <Link to="/about">
-                <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">About</li>
-            </Link>
-            <Link to="/sign-in">
-                <li className="text-slate-700 hover:underline cursor-pointer">Sign in</li>
-            </Link>
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+              About
+            </li>
+          </Link>
+          <Link to="/profile">
+            {currentUser ? (
+              <img src={currentUser.avatar} alt="Profile" className="rounded-full h-7 w-7 object-cover" />
+            ) : (
+              <li className="text-slate-700 hover:underline cursor-pointer">
+                Sign in
+              </li>
+            )}
+          </Link>
         </ul>
       </div>
     </header>
--- client/src/redux/user/userSlice.ts
+++ client/src/redux/user/userSlice.ts
@@ -1,34 +1,36 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-export interface UserSate {
-    currentUser: string | null;
-    error: string | null;
-    loading: boolean;
+export interface UserState {
+  currentUser: {
+    avatar: string;
+  } | null;
+  error: string | null;
+  loading: boolean;
 }
 
-const initialState: UserSate = {
-    currentUser: null,
-    error: null,
-    loading: false,
-}
+const initialState: UserState = {
+  currentUser: null,
+  error: null,
+  loading: false,
+};
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        signInStart: (state) => {
-            state.loading = true;
-        },
-        signInSuccess: (state, action: PayloadAction<string>) => {
-            state.currentUser = action.payload;
-            state.loading = false;
-            state.error = null;
-        },
-        signInFailure: (state, action: PayloadAction<string>) => {
-            state.error = action.payload;
-            state.loading = false;
-        }
-    }
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+    },
+    signInSuccess: (state, action: PayloadAction<{ avatar: string }>) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action: PayloadAction<string>) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
 });
 
 export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
