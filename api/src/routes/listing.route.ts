da6d56ae49d275fd4950b54288e5bd27e1535175
baranero <jakub.baran2@gmail.com>
2023-10-31 15:32:32 +0100

    create update listing api route

--- api/src/routes/listing.route.ts
+++ api/src/routes/listing.route.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from "express";
-import { createListing, deleteListing } from "../controllers/listing.controller";
+import { createListing, deleteListing, updateListing } from "../controllers/listing.controller";
 import { verifyUser } from "../utils/verifyUser";
 
 const router = express.Router();
 
 router.post('/create', verifyUser, createListing);
 router.delete('/delete/:id', verifyUser, deleteListing);
+router.post('/update/:id', verifyUser, updateListing);
 
 export default router;
