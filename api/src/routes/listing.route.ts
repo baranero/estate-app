43a6be49884854c00028db6088310f26296b66bc
baranero <jakub.baran2@gmail.com>
2023-10-31 23:16:10 +0100

    complete update listing functionality

--- api/src/routes/listing.route.ts
+++ api/src/routes/listing.route.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express";
-import { createListing, deleteListing, updateListing } from "../controllers/listing.controller";
+import { createListing, deleteListing, updateListing, getListing } from "../controllers/listing.controller";
 import { verifyUser } from "../utils/verifyUser";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/create', verifyUser, createListing);
 router.delete('/delete/:id', verifyUser, deleteListing);
 router.post('/update/:id', verifyUser, updateListing);
+router.get('/get/:id', getListing);
 
 export default router;
